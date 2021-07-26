5c2309df7a9cfae2c7c3dfa3e63b4e8a927fa353
dinhhop172 <dinhhop1702@gmail.com>
2021-07-26 16:33:48 +0700

    add controller frontend

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Slider;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    private $slider;
+    public function __construct(Slider $slider)
+    {
+        $this->slider = $slider;
+    }
+    public function index()
+    {
+        $sliders = $this->slider->get();
+        return view('frontend.home.home', compact('sliders'));
+    }
+}
